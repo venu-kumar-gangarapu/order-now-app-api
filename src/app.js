d04592793e0dd4fc97ea84cda62339c188495b90
Venu <venu16046016@gmail.com>
2026-08-20 22:32:20 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.options('/*any', cors());
+// app.options('/*any', cors());
 app.use(express.json())
 app.use('/ordernowApi/v1', routes);
 
